17b86e9f98855b03538cbb6d09dfbcec5c504f3d
Kaitlin Wiener <kaitwiener@gmail.com>
2015-11-05 14:26:47 -0500

    models article validates all necessary inputs, belongs to user, and has many comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ActiveRecord::Base
+  validates :location, presence: true
+  validates :longitude, presence: true
+  validates :latitude, presence: true
+  validates :body, presence: true
+  validates :date_traveled, presence: true
+
+  belongs_to :user, foreign_key: :user_id
+
+  has_many :comments, dependent: :destroy
 end
