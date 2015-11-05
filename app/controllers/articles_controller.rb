089507ed65ba837e02e464d9d073f5d8ad76cf51
Kaitlin Wiener <kaitwiener@gmail.com>
2015-11-05 14:27:20 -0500

    wrote index, create, update, and destroy methods for Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,36 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def create
+  @article = Article.new(article_params)
+  @article.user_id = 1
+
+  if @article.save
+
+    else
+      flash[:message] = @article.errors.full_messages.to_sentence
+    end
+    redirect_to articles_path
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+  end
+
+  def destroy
+  @article = Article.find(params[:id])
+
+  @article.destroy
+
+end
+
+private
+
+  def article_params
+    return params.require(:article).permit(:location, :latitude, :longitude, :body, :date_traveled)
+  end
 end
