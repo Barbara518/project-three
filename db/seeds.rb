9cb90028131027f9f2868ba588f5ce507e53df56
Barbara Rodriguez-Zamora <barbararodriguezzamora@gmail.com>
2015-11-06 10:13:41 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do
+  user = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password_digest: Faker::Internet.password
+  )
+  1.times do
+    user.articles.create(
+    location: Faker::Address.city,
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+    body: Faker::Lorem.paragraph,
+    date_traveled: Faker::Date.backward(700)
+    )
+  end
+end
