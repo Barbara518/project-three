253f879f680a436eca8eee08877daec86ab8b252
Kaitlin Wiener <kaitwiener@gmail.com>
2015-11-09 19:45:17 -0500

    Added $location to our articles controller in order to redirect once a new article is created

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -64,7 +64,7 @@ app.controller('SessionController', ['$http', '$scope', function($http, $scope) 
 ///////////////////////////////////////////////////////////////////////
 //////////////////////////Article Controller//////////////////////////
 /////////////////////////////////////////////////////////////////////
-app.controller('ArticlesController', ['$http', '$scope', function($http, $scope) {
+app.controller('ArticlesController', ['$http', '$scope', '$location', function($http, $scope, $location) {
   var controller = this;
   console.log(controller);
 
@@ -89,6 +89,7 @@ app.controller('ArticlesController', ['$http', '$scope', function($http, $scope)
     }).success(function(data){
       controller.newArticle = {};
       console.log(data);
+      $location.path("/articles");
     })
 
   }
